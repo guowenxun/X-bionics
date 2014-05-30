6e7fcc8569732e340686bf0ee0d3d9b9dd1d75ba
guowenxun <guowenxun@163.com>
2014-05-30 14:12:55 +0800

    a

--- src/com/imcore/wenxun/UI/SearchPager_activity.java
+++ src/com/imcore/wenxun/UI/SearchPager_activity.java
@@ -10,15 +10,26 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.GridView;
 import android.os.Build;
 
 public class SearchPager_activity extends Activity {
 
+	private GridView mGridView;
+	private Button productButton;
+	private EditText productEditText;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.searchpager_activity);
 
+		mGridView=(GridView) findViewById(R.id.product_search_gridview);
+		productButton=(Button) findViewById(R.id.product_find_button);
+		productEditText=(EditText) findViewById(R.id.product_search_edittext);
+		
 		
 		}
 	}
