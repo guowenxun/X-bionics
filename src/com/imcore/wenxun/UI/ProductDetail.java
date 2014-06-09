f5950a14b8e9de68e2fb9dbf43202413f6a4067e
guowenxun <guowenxun@163.com>
2014-06-09 18:05:15 +0800

    aa

---
+++ src/com/imcore/wenxun/UI/ProductDetail.java
@@ -0,0 +1,19 @@
+package com.imcore.wenxun.UI;
+
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBarActivity;
+
+public class ProductDetail extends ActionBarActivity{
+	
+	private ViewPager mViewPager;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.productdetail);
+		
+		
+	}
+
+}
---
+++ src/com/imcore/wenxun/UI/ProductDetailGridview.java
@@ -0,0 +1,19 @@
+package com.imcore.wenxun.UI;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ProductDetailGridview extends Fragment{
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View view=inflater.inflate(R.layout.productdetialgridview_activity, null);
+		
+		
+		return view;
+	}
+}
